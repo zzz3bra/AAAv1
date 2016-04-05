bcdc5f9b07718d835786a4e22deb9344f998b39b
zzz3bra <z3vs@yandex.ru>
2016-04-05 23:42:42 +0300

    DB preparation; notification stub
    Simple DB interaction, stubs for future DB-related classes and
    notification manager

---
+++ AAAv1/Models/UserBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AAAv1.Models
+{
+    public class UserBase
+    {
+        protected UserBase() { }
+        public static UserBase Instance
+        {
+            get
+            {
+                return UserBaseCreator.Instance;
+            }
+        }
+
+        private sealed class UserBaseCreator
+        {
+            private static readonly UserBase instance = new UserBase();
+            public static UserBase Instance
+            {
+                get { return instance; }
+            }
+        }
+        public UserRecord GetUserByCredentials(string account, string password)
+        {
+            //запрос в базу, поиск юзера и возврат его в виде объекта
+            return new UserRecord(1, "AGL", new List<ADS>(), new List<Notification>());
+        }
+        public void RemoveUserAccountById(int UserID)
+        {
+            //удаление юзера из базы
+        }
+        public void UpdateUserInfo(int UsedID, List<ADS> FavouriteADS, List<Notification> Notification)
+        {
+            //изменение предпочтений пользователя, пароль пока не трогаем
+        }
+        public List<Notification> GetAllNotifications()
+        {
+            //получаем все уведомления для класса УВЕДОМИТЕЛЬ который собстенно и будет уведомлять
+            return new List<Notification>();
+        }
+    }
+}
---
+++ AAAv1/Models/UserRecord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AAAv1.Models
+{
+    public class UserRecord
+    {
+        public int id { get; private set; }
+        public string Name { get; private set; }
+        public List<ADS> FavouriteADS { get; private set; }
+        public List<Notification> Notifications { get; private set; }
+        public void AddNotification(Notification notification)
+        {
+            Notifications.Add(notification);
+        }
+        public void AddFavouriteADS(ADS ad)
+        {
+            FavouriteADS.Add(ad);
+        }
+        public void RemoveNotification(Notification notification)
+        {
+            Notifications.Remove(notification);
+        }
+        public void RemoveFavouriteADS(ADS ad)
+        {
+            FavouriteADS.Remove(ad);
+        }
+        public UserRecord(int id, string Name, List<ADS> FavouriteADS, List<Notification> Notification)
+        {
+            this.id = id;
+            this.Name = Name;
+            this.FavouriteADS = FavouriteADS;
+            this.Notifications = Notifications;
+        }
+    }
+}
